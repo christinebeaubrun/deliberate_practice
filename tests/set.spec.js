15eddada7549da166e81720494034b1ea5e33fb9
christinebeaubrun <christinebeaubrun@gmail.com>
2015-11-11 07:49:14 -0800

    Added basic functionality to set fn and test

--- tests/set.spec.js
+++ tests/set.spec.js
@@ -14,3 +14,44 @@ function assert_not_eq(a, b) {
   return !(a === b);
 }
 
+var Set = function () {
+  var collection = [];
+  return {
+    add: function (arg) {
+      if ( arg ) {
+        collection.push(arg);
+      }
+    },
+    isMember: function (arg) {
+      var isPresent = false;
+      if ( arg ) {
+        collection.forEach( function ( member ) {
+          isPresent = ( member === arg ) ? true : false;
+        });
+        return isPresent;
+      }
+    }
+  };
+};
+
+test( 'should have an add method', function () {
+  var set = Set();
+  return assert_eq( typeof set.add, "function");
+});
+
+test( 'should have an isMember method', function () {
+  var set = Set();
+  return assert_eq( typeof set.isMember, "function");
+});
+
+test( 'should return true if key is member of set', function () {
+  var set = Set();
+  set.add(7);
+  return assert_eq( set.isMember(7), true);
+});
+
+test( 'should return false if key is not a member of set', function () {
+  var set = Set();
+  set.add(8);
+  return assert_eq( set.isMember(1), false);
+});
